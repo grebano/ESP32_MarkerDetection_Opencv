728e16ee79e0db978650f7733480ffcba725f02d
grebano <simonemaschio01@gmail.com>
2023-06-21 15:23:33 +0200

    added empty esp-idf project to develop esp32 code

---
+++ sqrDetection_z_Porting/main/main.cpp
@@ -0,0 +1,32 @@
+#undef EPS      // specreg.h defines EPS which interfere with opencv
+#include "opencv2/core.hpp"
+#include "opencv2/imgproc.hpp"
+#include "opencv2/imgcodecs.hpp"
+#define EPS 192
+
+#include <esp_log.h>
+#include <esp_err.h>
+#include <esp_timer.h>
+
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+#include <esp_freertos_hooks.h>
+#include <iostream>
+#include <map>
+
+//#include "system.h"
+
+using namespace cv;
+
+extern "C" {
+void app_main(void);
+}
+
+
+#define TAG "main"
+
+
+void app_main(void)
+{
+    ESP_LOGI(TAG, "Starting main task");
+}
